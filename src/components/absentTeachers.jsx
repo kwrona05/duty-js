79a2c9ef3efb68419332b8a2ca38cd077984a297
kwrona05 <kwrona05@gmail.com>
2025-04-02 14:29:23 +0200

    Create component to mark absent teachers

---
+++ src/components/absentTeachers.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./absentTeachers.scss";
+
+function AbsentTeachers({ teachers, setTeachers }) {
+  const [isFormOpen, setIsFormOpen] = useState(false); // Kontrola rozwinięcia formularza
+  const [absentTeachers, setAbsentTeachers] = useState([]);
+
+  const toggleForm = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+
+  const toggleAbsent = (teacherId) => {
+    const updatedTeachers = teachers.map((teacher) =>
+      teacher.id === teacherId
+        ? { ...teacher, isAvailable: !teacher.isAvailable }
+        : teacher
+    );
+
+    setTeachers(updatedTeachers);
+  };
+
+  return (
+    <div className="absent-teachers">
+      <button onClick={toggleForm} className="toggle-form-btn">
+        {isFormOpen ? "Hide Form" : "Mark Absent Teachers"}
+      </button>
+
+      {/* Jeśli formularz jest rozwinięty, wyświetl go */}
+      {isFormOpen && (
+        <div className="form-container">
+          <h3>Mark Absent Teachers</h3>
+          <ul>
+            {teachers.map((teacher) => (
+              <li key={teacher.id} className="teacher-item">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={!teacher.isAvailable}
+                    onChange={() => toggleAbsent(teacher.id)}
+                  />
+                  {teacher.name} {teacher.surname}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default AbsentTeachers;
